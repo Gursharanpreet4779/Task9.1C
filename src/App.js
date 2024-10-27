e4aee1e9c908d20e0f2838929c69ab4965a52ade
Gursharanpreet Singh Sidhu <gurisidhu25@Gursharanpreets-MacBook-Pro.local>
2024-10-27 15:14:03 +0530

    guri

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './Signup';
+import Login from './Login';
+import Home from './home';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
