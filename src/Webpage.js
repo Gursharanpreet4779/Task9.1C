ab899600a6757f17d809e60749c3f140260545aa
Gursharanpreet Singh Sidhu <gurisidhu25@Gursharanpreets-MacBook-Pro.local>
2024-10-31 13:38:34 +0530

    guri

--- src/Webpage.js
+++ src/Webpage.js
@@ -14,23 +14,6 @@ function HomePage() {
           <Link className="log" to="/login">Log In</Link>
         </div>
 
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="query">Query</Link>
-            </li>
-          </ul>
-        </nav> */}
-
       </header>
     </div>
   );
